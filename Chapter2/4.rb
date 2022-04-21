cdc0e0e1d2bfe9d2887067ebd2e14c17c9373f54
christophe126 <christophe126@gmail.com>
2022-04-21 14:00:32 +0200

    Ternary operator

---
+++ Chapter2/4.rb
@@ -0,0 +1,10 @@
+# Chapter 2. Choose the Right Control Structure
+# Ternary operator ?
+
+
+file = all ? 'specs' : 'latest_specs'
+@firstname = '' unless @firstname
+@firstname ||=''
+count += 1
+count = count + 1
+@first_name = @first_name || ''
